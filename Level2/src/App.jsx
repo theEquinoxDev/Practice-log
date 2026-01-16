dd8dc5fffcd56ca68560de5fc60b7062acaab103
Aditya <adityasuri11ss@gmail.com>
2026-01-16 21:51:59 +0530

    small components commit

---
+++ Level2/src/App.jsx
@@ -0,0 +1,19 @@
+import Accordion from './components/Accordion'
+import Counter from './components/Counter'
+import LightDark from './components/LightDark'
+import Notes from './components/Notes'
+import ToDo from './components/ToDo'
+
+const App = () => {
+  return (
+    <div>
+      {/* <Counter/> */}
+      {/* <ToDo/> */}
+      {/* <Notes/> */}
+      {/* <Accordion/> */}
+      <LightDark/>
+    </div>
+  )
+}
+
+export default App
