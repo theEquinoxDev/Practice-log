6b33cc84939f435a675429d586f6ab385e454f6f
Aditya <adityasuri11ss@gmail.com>
2026-01-19 22:24:48 +0530

    forgot to commit all this

---
+++ Level3/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import MultiStepForm from './components/MultiStepForm'
+import TabComponent from './components/TabComponent'
+
+function App() {
+
+  return (
+    <div>
+      {/* <MultiStepForm/> */}
+      <TabComponent/>
+    </div>
+  )
+}
+
+export default App
